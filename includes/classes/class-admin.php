1d70b055cf2581d51ad004da59ca1abd67a70ec6
Aaron Brazell <aaron@technosailor.com>
2017-02-07 18:40:35 +0000

    Wire up user profile UI and meta

--- includes/classes/class-admin.php
+++ includes/classes/class-admin.php
@@ -3,8 +3,22 @@ namespace WPPC\Admin;
 
 class WPPC_Admin {
 
-    public static function add_profile_meta() {
-        wp_nonce_field( 'wppc-publishing-controls' );
+    public static function save_profile_meta( $user_id ) {
+
+        if( ! check_admin_referer( 'wppc-publishing-controls', 'wppc-publishing-controls-nonce') ) {
+            return false;
+        }
+
+        if( ! empty( $_POST[ 'wppc-restrict-publishing' ] ) ) {
+            update_user_meta( (int) $user_id, 'wppc-disallow-publish', true );
+        } else {
+            update_user_meta( (int) $user_id, 'wppc-disallow-publish', false );
+        }
+    }
+
+    public static function add_profile_meta( $user ) {
+        wp_nonce_field( 'wppc-publishing-controls', 'wppc-publishing-controls-nonce' );
+        $disallowed = ( get_user_meta( $user->ID, 'wppc-disallow-publish', true ) ) ? 'on' : '';
         ?>
         <h2><?php _e( 'Publishing Control', 'wppc' ); ?></h2>
         <table class="form-table">
@@ -16,7 +30,7 @@ class WPPC_Admin {
                         </label>
                     </th>
                     <td>
-                        <input type="checkbox" id="wppc-restrict-publishing" <?php echo ( ! current_user_can( 'manage_options' ) ) ? 'disabled="disabled"' : ''; ?>>
+                        <input type="checkbox" id="wppc-restrict-publishing" name="wppc-restrict-publishing" <?php checked( $disallowed, 'on' ) ?>>
                     </td>
                 </tr>
             </tbody>
--- includes/setup.php
+++ includes/setup.php
@@ -16,4 +16,6 @@ function hooks() {
     add_action( 'user_has_cap', '\WPPC\Capabilities\limit_publish_capability', 10, 3 );
     add_action( 'edit_user_profile', [ '\WPPC\Admin\WPPC_Admin', 'add_profile_meta' ] );
     add_action( 'show_user_profile', [ '\WPPC\Admin\WPPC_Admin', 'add_profile_meta' ] );
+    add_action( 'personal_options_update', [ '\WPPC\Admin\WPPC_Admin', 'save_profile_meta' ] );
+    add_action( 'edit_user_profile_update', [ '\WPPC\Admin\WPPC_Admin', 'save_profile_meta' ] );
 }
